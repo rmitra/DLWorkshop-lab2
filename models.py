74df0af9fe7f7ad27555b027113d39f035ae15bd
Rishabh Dabral <rishuud@gmail.com>
2020-03-04 12:36:14 +0530

    Update models.py

--- models.py
+++ models.py
@@ -22,38 +22,22 @@ class Lifter(nn.Module):
         self.num_joints = num_joints
         self.hidden_dim = hidden_dim
 
-        # Shared features between the Pose and Camera blocks
-        self.shared_feats = nn.Sequential(
+        # We wrap the Lifter architecture inside a Sequential container
+        self.pose_reg = nn.Sequential(
                             nn.Linear(2*self.num_joints, self.hidden_dim),
                             nn.LeakyReLU(0.1),
                             ResBlock(self.hidden_dim)
-                            )
-
-
-        # The next blocks are meant for 3D pose regression
-        self.pose_reg = nn.Sequential(
-                        ResBlock(self.hidden_dim),
-                        ResBlock(self.hidden_dim),
-                        nn.Linear(self.hidden_dim, self.hidden_dim),
-                        nn.LeakyReLU(0.1),
-                        nn.Linear(self.hidden_dim, 3*self.num_joints)
-                        )
-
-        # The next blocks are meant for camera regression
-        self.cam_reg = nn.Sequential(
-                        ResBlock(self.hidden_dim),
-                        ResBlock(self.hidden_dim),
-                        nn.Linear(self.hidden_dim, 6)
+                            ResBlock(self.hidden_dim),
+                            ResBlock(self.hidden_dim),
+                            nn.Linear(self.hidden_dim, self.hidden_dim),
+                            nn.LeakyReLU(0.1),
+                            nn.Linear(self.hidden_dim, 3*self.num_joints)
                         )
 
 
-    def forward(self, input, cams):
-        # input: n x n_joints*2
-        # cams: n x 2 x 3
-        backbone = self.shared_feats(input)
-
-        poses_3d = self.pose_reg(backbone)
-        # cams = self.cam_reg(backbone)
+    def forward(self, input):
+        # input: b x n_joints*2
+        poses_3d = self.pose_reg(input)
 
         return poses_3d
 
